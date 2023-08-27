ab68af5eb809e17b50653f39808c0fff136c8f94
GPLme <524019351@qq.com>
2023-08-27 11:27:39 +0000

    JB检验
    Signed-off-by: GPLme <524019351@qq.com>

--- s1.py
+++ s1.py
@@ -1,4 +1,29 @@
 import pandas as pd
+import numpy as np
+import scipy.stats as stats
+from statsmodels.tsa.stattools import adfuller
+
+def self_JBtest(y):
+    # 样本规模n
+    n = y.size
+    y_ = y - y.mean()
+    """
+    M2:二阶中心钜
+    skew 偏度 = 三阶中心矩 与 M2^1.5的比
+    krut 峰值 = 四阶中心钜 与 M2^2 的比
+    """
+    M2 = np.mean(y_**2)
+    skew =  np.mean(y_**3)/M2**1.5
+    krut = np.mean(y_**4)/M2**2
+    """
+    计算JB统计量，以及建立假设检验
+    """
+    JB = n*(skew**2/6 + (krut-3 )**2/24)
+    pvalue = 1 - stats.chi2.cdf(JB,df=2)
+    # print("偏度：",stats.skew(y),skew)
+    # print("峰值：",stats.kurtosis(y)+3,krut)
+    print("JB检验：",stats.jarque_bera(y))
+    return np.array([JB,pvalue])
 
 sheets = ['SSEC', 'SCI', 'FTSE100', 'DAX30', 'CAC40']
 ret = {}
@@ -10,6 +35,7 @@ for sheet in sheets:
     for i in range(1, len(close)):
         rate = (close[i] - close[i-1]) / close[i-1]
         rates.append(rate)
+    self_JBtest(rates) # JB检验
     ret[sheet] = rates
 
 saveDf = pd.DataFrame(ret)
